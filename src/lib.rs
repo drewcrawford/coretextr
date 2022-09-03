737475900fb57c4f21abc77da36b32591d57b4b7
Drew Crawford <drew@sealedabstract.com>
2022-09-02 22:17:37 -0500

    Add StrongCell typeref

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,7 @@ mod ctfontmanager;
 pub use ctfontdescriptor::*;
 pub use ctfontmanager::*;
 
+pub type StrongCell<T> = core_foundationr::StrongCell<T>;
+
 #[link(name = "CoreText", kind = "framework")]
 extern "C" { }
